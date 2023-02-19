f9702b14c6ea1bc971c5531540b64e98a153fe16
SIARRAS Stéphane <siarras.stephane@gmail.com>
2023-02-18 23:17:39 -0400

    menus ok

---
+++ src/components/check/check.js
@@ -0,0 +1,17 @@
+import './check.css';
+// import yoga from "./check/assets/yoga.png";
+// import yoga from "./assets/yoga.png";
+const Check = function (props) {
+
+    console.log("Chek",props.checkIcon);
+    return (
+        <>
+            <div className='Background-icon'>
+                <img className="check_icon" src={props.checkIcon} alt={props.altChekIcon} />
+                {/* <img className="check_icon" src="./assets/bike.png" alt={props.altChekIcon} /> */}
+            </div>
+        </>
+    )
+
+}
+export default Check;
--- src/components/leftbar.js
+++ src/components/leftbar.js
@@ -1,12 +1,28 @@
 import { NavLink } from "react-router-dom";
 import Navigation from "./navigation";
+import Check from "./check/check";
+
+import yoga from "./check/assets/yoga.png";
+import swim from "./check/assets/swim.png";
+import bike from "./check/assets/bike.png";
+import force from "./check/assets/force.png";
 
 const Leftbar = function (props) {
 
     return (
-            <div className="menu_left">
-                {/* <Navigation type="fa-solid fa-person-swimming"/> */}
+        <div className="menu_left">
+            {/* <Navigation type="fa-solid fa-person-swimming"/> */}
+            {/* <Check checkIcon=".check/assets/yoga.png"/> */}
+            <div className="checkIcon_total">
+                <Check checkIcon={yoga} />
+                <Check checkIcon={swim} />
+                <Check checkIcon={bike} />
+                <Check checkIcon={force} />
             </div>
+            <div>
+                <p className="copyright"><small> Copyright Sportsee 2020</small></p>
+            </div>
+        </div>
     )
 
 }
