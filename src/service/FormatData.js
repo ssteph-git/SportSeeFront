dcc0f168a68b108bb2dc8aa5873369646ba1d876
SIARRAS Stéphane <siarras.stephane@gmail.com>
2023-02-21 22:48:46 -0400

    chart radar traduit en français

--- src/service/FormatData.js
+++ src/service/FormatData.js
@@ -1,101 +1,105 @@
-class FormatData{
+class FormatData {
     /**
      * 
      * @param {*} e 
      * @returns 
      */
-    Bar(e){
+    Bar(e) {
         e.map(x => {
-            x.day = String(x.day[x.day.length-1])//récupération des jours dans la date: puis conversion en string
-            x.kilogram =  parseInt(x.kilogram, 10)
-            x.calories =  parseInt(x.calories, 10)
+            x.day = String(x.day[x.day.length - 1])//récupération des jours dans la date: puis conversion en string
+            x.kilogram = parseInt(x.kilogram, 10)
+            x.calories = parseInt(x.calories, 10)
             return x;
         })
         return e;
     };
 
-    Line(e){
-        
+    Line(e) {
+
         e.map(x => {
             switch (x.day) {
                 case 1:
                     x.day = "L"
-                  break;
+                    break;
                 case 2:
                     x.day = "M"
-                break;
+                    break;
                 case 3:
                     x.day = "M"
-                break;
+                    break;
                 case 4:
                     x.day = "J"
-                break;
+                    break;
                 case 5:
                     x.day = "V"
-                break;
+                    break;
                 case 6:
                     x.day = "S"
-                break;
+                    break;
                 case 7:
                     x.day = "D"
-                break;
-                // default: x.day = "";
-              }
-            
-            // x.day[i] = String(x.day)
-            x.sessionLength =  parseInt(x.sessionLength, 10)
+                    break;
+                default: return "";
+            }
+
+            x.sessionLength = parseInt(x.sessionLength, 10)
             return x;
         })
         return e;
     };
 
-    radar(e){
+    radar(e) {
+        function translateFrench(e) {
+            switch (e) {
+                case 'cardio': return "Cardio";
+                case 'energy': return "Energie";
+                case 'endurance': return "Endurance";
+                case 'strength': return "Force";
+                case 'speed': return "Vitesse";
+                case 'intensity': return "Intensité";
+                default: return "";
+            }
+        }
+
         e.data.map((x) => {
-            // x.value = parseInt(x.value, 10);
-              switch (x.kind) {
+            switch (x.kind) {
                 case 1:
-                    x.kind = e.kind[1];
-                  break;
+                    x.kind = translateFrench(e.kind[1]);
+                    break;
                 case 2:
-                    // x.kind = "energy"
-                    x.kind = e.kind[2];
-                break;
+                    x.kind = translateFrench(e.kind[2]);
+                    break;
                 case 3:
-                    // x.kind = "endurance"
-                    x.kind = e.kind[3];
-                break;
+                    x.kind = translateFrench(e.kind[3]);
+                    break;
                 case 4:
-                    // x.kind = "strength"
-                    x.kind = e.kind[4];
-                break;
+                    x.kind = translateFrench(e.kind[4]);
+                    break;
                 case 5:
-                    // x.kind = "speed"
-                    x.kind = e.kind[5];
-                break;
+                    x.kind = translateFrench(e.kind[5]);
+                    break;
                 case 6:
-                    // x.kind = "intensity"
-                    x.kind = e.kind[6];
-                break;
-                // default: 
-                // x.kind = "";
-              }
-            
+                    x.kind = translateFrench(e.kind[6]);
+                    break;
+                default: return "";
+            }
+
 
             return x;
         })
-       return e;
+        return e;
     };
 
-    pie(e){
-        if(isNaN( e.score)){
+    pie(e) {
+        if (isNaN(e.score)) {
             e.todayScore = parseFloat(e.todayScore);
             return e.todayScore;
-        }else{
+        } else {
             e.score = parseFloat(e.score);
             return e.score;
         }
     };
 
-    
+
 }
 export default FormatData;
